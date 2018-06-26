3c8f5e64d7a8a65c355def1284c44fa585e387c3
kalex19 <klewis@rockymountainprep.org>
2018-06-26 00:23:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-function shout(string) { console.log('hello')
-  return string.toUpperCase ( )
+function shout(string) { console.log (string).toUpperCase ( )
 } 
 
 function whisper(string) { return string.toLowerCase () }
